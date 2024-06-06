280807b164aa3c6771ff5b40de9de2142c5677e9
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-06 10:44:47 +0200

    Create Problem #017-Guess a 3 Letter Password.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Level_2/Problem #017-Guess a 3 Letter Password.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+
+using namespace std; 
+
+string ReadPassword() 
+{ 
+    string Password;  
+
+    cout << "Please enter a 3-Letter Password (all capital)?\n";     
+    cin >> Password; 
+
+    return Password; 
+}
+
+bool GuessPassword(string OriginalPassword)
+{ 
+    cout << "\n"; 
+    string word = ""; 
+    int Counter = 0;
+
+    for (int i = 65; i <= 90; i++) 
+    { 
+        for (int j = 65; j <= 90; j++) 
+        { 
+            for (int k = 65; k <= 90; k++) 
+            { 
+                word = word + char(i);                 
+                word = word + char(j);                 
+                word = word + char(k);  
+
+                Counter++;                 
+                cout << "Trial [" << Counter << "] : ";      
+                cout << word << endl;
+
+                if (word == OriginalPassword) 
+                { 
+                    cout << "\nPassword is " << word << "\n"; 
+                    cout << "Found after ";                     
+                    cout << Counter << " Trial(s)\n"; 
+                    return true; 
+                }                 
+                word = "";
+                
+            } 
+        }         
+    }
+    return false;
+}
+
+int main() 
+{ 
+    GuessPassword(ReadPassword());
+   
+    return 0; 
+}
