e4ff605cbcbb1e61cd0b7ad318724c06218b5a98
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-06 14:41:34 +0200

    Create problem #023-fill array with random numbers from 1 to 10.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Level_2/problem #023-fill array with random numbers from 1 to 10.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std; 
+
+
+int RandomNumber(int From, int To)
+{
+    //Function to generate a random number
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& arrLength) 
+{ 
+    cout << "\nEnter number of elements:\n";     
+    cin >> arrLength; 
+
+    for (int i = 0; i < arrLength; i++) 
+        arr[i] = RandomNumber(1, 100); 
+}
+
+void PrintArray(int arr[100], int arrLength) 
+{ 
+    for (int i = 0; i < arrLength; i++)         
+        cout << arr[i] << " ";  
+
+    cout << "\n"; 
+}
+
+
+int main() 
+{ 
+    //Seeds the random number generator in C++, called only once    
+    srand((unsigned)time(NULL)); 
+
+    int arr[100], arrLength;     
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "\narray Elements: "; 
+    PrintArray(arr, arrLength); 
+
+    return 0; 
+}
