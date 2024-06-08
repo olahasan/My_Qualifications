425a1789d6c789e44d0103328f582f8944e05196
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-08 15:18:55 +0200

    Create Problem #030-Count Letters.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Level_3/Problem #030-Count Letters.cpp
@@ -0,0 +1,52 @@
+#include<string>
+#include<iostream>
+using namespace std; 
+
+
+
+string  ReadString()
+{
+    string S1;
+
+    cout << "Please Enter Your String?\n";
+    getline(cin, S1);
+
+    return S1;
+}
+
+
+char  ReadChar()
+{
+    char Ch1;
+
+    cout << "Please Enter a Character?\n";
+    cin >> Ch1;
+
+    return Ch1;
+}
+
+
+short  CountLetter(string S1, char Letter) 
+{ 
+    short Counter = 0;
+    
+    for (short i = 0; i < S1.length(); i++) 
+    { 
+        //if (toupper(S1[i]) == toupper(Letter))
+        if (S1[i] == Letter)
+            Counter++;
+    } 
+    return Counter; 
+}
+
+
+
+int main() 
+{ 
+    string S1 = ReadString(); 
+    char Ch1 = ReadChar();   
+    
+    cout << "\nLetter \'" << Ch1 << "\' Count = " << CountLetter(S1, Ch1);
+
+    system("pause>0"); 
+} 
