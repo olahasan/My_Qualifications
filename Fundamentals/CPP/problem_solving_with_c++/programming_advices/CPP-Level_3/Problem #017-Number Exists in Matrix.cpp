9e2b861465124185db62f8c1461262d567854404
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-08 10:02:03 +0200

    Create Problem #017-Number Exists in Matrix.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Level_3/Problem #017-Number Exists in Matrix.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+using namespace std; 
+
+void PrintMatrix(int arr[3][3], short Rows, short Cols) 
+{ 
+    for (short i = 0; i < Rows; i++) 
+    { 
+        for (short j = 0; j < Cols; j++) 
+        { 
+            //cout << setw(3) << arr[i][j] << "     "; 
+            printf(" %0*d   ", 2, arr[i][j]);
+        }           
+        cout << "\n"; 
+    } 
+}
+
+
+bool IsNumberInMatrix(int Matrix1[3][3], int Number, short Rows, short Cols) 
+{ 
+    short NumberCount = 0; 
+    
+    for (short i = 0; i < Rows; i++) 
+    { 
+        for (short j = 0; j < Cols; j++) 
+        { 
+            if (Matrix1[i][j] == Number) 
+            { 
+                return true; 
+            }; 
+        } 
+    }     
+    return false; 
+}
+
+int main() 
+{    
+    int Matrix1[3][3] = { 
+                            {77,5,12},
+                            {22,20,1},
+                            {1,0,9} 
+                        };
+    
+    cout << "\nMatrix1:\n";      
+    PrintMatrix(Matrix1, 3, 3);    
+    
+    int Number;      
+    cout << "\nPlease Enter the number to look for in matrix? ";     
+    cin >> Number; 
+    
+     if (IsNumberInMatrix(Matrix1, Number, 3, 3))           
+        cout<<"\nYes it is there.\n"; 
+    else          
+        cout<<"\nNo: It's NOT there.\n";
+
+    system("pause>0"); 
+}
