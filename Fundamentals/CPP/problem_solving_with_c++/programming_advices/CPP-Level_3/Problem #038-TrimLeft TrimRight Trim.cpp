20aeb332ded18d02072d6ade55f5b9d2c3537bad
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-08 17:49:27 +0200

    Create Problem #038-TrimLeft TrimRight Trim.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Level_3/Problem #038-TrimLeft TrimRight Trim.cpp
@@ -0,0 +1,58 @@
+#include<string>
+#include<iostream>
+using namespace std; 
+
+
+string  ReadString()
+{
+    string S1;
+
+    cout << "Please Enter Your String?\n";
+    getline(cin, S1);
+
+    return S1;
+}
+
+string TrimLeft(string S1) 
+{ 
+    for (short i = 0; i < S1.length(); i++) 
+    { 
+        if (S1[i] != ' ') 
+        { 
+            return S1.substr(i, S1.length() - i); 
+        } 
+    } 
+    return ""; 
+}
+
+string TrimRight(string S1) 
+{ 
+    for (short i = S1.length() - 1; i >= 0; i--) 
+    { 
+        if (S1[i] != ' ') 
+        { 
+            return S1.substr(0, i + 1); 
+        } 
+    } 
+
+    return""; 
+}
+
+
+string Trim(string S1) 
+{ 
+    return (TrimLeft(TrimRight(S1))); 
+}
+
+int main() 
+{ 
+    string S1 = "      Mohammed Abu-Hahdoud      ";   
+    
+    cout << "\nString     = " << S1; 
+
+    cout << "\n\nTrim Left  = " << TrimLeft(S1);    
+    cout << "\nTrim Right = " << TrimRight(S1);    
+    cout << "\nTrim       = " << Trim(S1);
+
+    system("pause>0"); 
+}
