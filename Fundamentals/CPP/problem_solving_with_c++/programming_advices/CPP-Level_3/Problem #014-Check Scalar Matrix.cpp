628bf092cb7708a863c01866a1beca33c1a32621
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-08 09:46:36 +0200

    Update Problem #014-Check Scalar Matrix.cpp

--- Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Level_3/Problem #014-Check Scalar Matrix.cpp
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Level_3/Problem #014-Check Scalar Matrix.cpp
@@ -3,24 +3,6 @@
 #include<iomanip>
 using namespace std; 
 
-int RandomNumber(int From, int To)
-{
-    //Function to generate a random number
-    int randNum = rand() % (To - From + 1) + From;
-    return randNum;
-}
-
-void FillMatrixWithRandomNumbers(int arr[3][3], short Rows, short Cols)
-{
-    for (short i = 0; i < Rows; i++)
-    {
-        for (short j = 0; j < Cols; j++)
-        {
-            arr[i][j] = RandomNumber(1, 10);
-        }
-    }
-}
-
 void PrintMatrix(int arr[3][3], short Rows, short Cols) 
 { 
     for (short i = 0; i < Rows; i++) 
