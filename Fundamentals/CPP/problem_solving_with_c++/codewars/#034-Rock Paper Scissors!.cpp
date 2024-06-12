ee56978e075091c97b6ca2486ad1cd29ef2a9085
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-12 06:13:52 +0200

    Create #034-Rock Paper Scissors!.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/codewars/#034-Rock Paper Scissors!.cpp
@@ -0,0 +1,11 @@
+#include <string>
+
+std::string rps(const std::string& p1, const std::string& p2)
+{
+    if(p1 == p2)
+      return "Draw!";
+    else if(p1 == "scissors" && p2 == "paper" || p1 == "rock" && p2 == "scissors" || p1 == "paper" && p2 == "rock")
+      return "Player 1 won!";
+    else
+      return "Player 2 won!";
+}
