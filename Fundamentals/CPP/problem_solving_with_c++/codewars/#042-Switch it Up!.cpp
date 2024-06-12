d9c9de83042126746a247da9dc729a8a7152d50a
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-12 06:51:50 +0200

    Create #042-Switch it Up!.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/codewars/#042-Switch it Up!.cpp
@@ -0,0 +1,6 @@
+#include <string>
+
+std::string switch_it_up(int number){
+  std::string words[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+  return words[number];
+}
