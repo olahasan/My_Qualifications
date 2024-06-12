929bfc8df855e7cbb939ceede3ab8bdaf9f438bb
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-12 08:35:16 +0200

    Create #083-Thinkful – Logic Drills: Traffic Light.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/codewars/#083-Thinkful – Logic Drills: Traffic Light.cpp
@@ -0,0 +1,16 @@
+#include <string>
+
+std::string update_light(std::string current) {
+  if (current == "green")
+  {
+    return "yellow";
+  }
+  if (current == "yellow")
+    {
+    return "red";
+  }
+  if (current == "red")
+    {
+    return "green";
+  }
+}
