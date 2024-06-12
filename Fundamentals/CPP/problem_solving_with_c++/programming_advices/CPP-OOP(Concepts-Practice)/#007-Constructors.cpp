47fefc9c3454972b5d25225b55b8d7d22564e8e0
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-12 14:29:18 +0200

    Create #007-Constructors.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-OOP(Concepts-Practice)/#007-Constructors.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+using namespace std;
+ 
+class clsAddress
+{ 
+private: 
+     string _AddressLine1; 
+     string _AddressLine2; 
+     string _POBox; 
+     string _ZipCode; 
+
+public :     
+    clsAddress(string AddressLine1, string AddressLine2, string POBox, string ZipCode)
+    { 
+        _AddressLine1 = AddressLine1;  
+        _AddressLine2 = AddressLine2;    
+        _POBox = POBox;    
+        _ZipCode = ZipCode;   
+    } 
+    
+    void SetAddressLine1(string AddressLine1) 
+    { 
+        _AddressLine1 = AddressLine1; 
+    }
+    string AddressLine1()   
+    { 
+        return _AddressLine1;
+    } 
+
+
+    void SetAddressLine2(string AddressLine2)  
+    { 
+        _AddressLine2 = AddressLine2;
+    } 
+    string AddressLine2()  
+    { 
+        return _AddressLine2;  
+    } 
+
+
+    void SetPOBox(string POBox)
+    { 
+        _POBox = POBox;
+    }
+    string POBox()
+    {
+        return _POBox;
+    }
+
+
+    void SetZipCode(string ZipCode) 
+    { 
+        _ZipCode = ZipCode; 
+    }
+    string ZipCode() 
+    { 
+        return _ZipCode;
+    } 
+
+
+    void Print()
+    {
+        cout << "\nAddress Details:\n";   
+        cout << "------------------------";      
+        cout << "\nAddressLine1: " << _AddressLine1 << endl;  
+        cout << "AddressLine2: " << _AddressLine2 << endl;      
+        cout << "POBox       : " << _POBox << endl;         
+        cout << "ZipCode     : " << _ZipCode << endl; 
+    } 
+}; 
+
+int main() 
+{ 
+    clsAddress Address1("Queen Ola Street", "B 303 ", "11192", "5555");  
+    Address1.Print();  
+
+    system("pause>0"); 
+    return 0; 
+}
