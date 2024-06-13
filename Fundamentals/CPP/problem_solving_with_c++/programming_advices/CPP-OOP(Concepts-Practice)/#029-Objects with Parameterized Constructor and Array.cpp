4309d39b77b34d774a8613ae9beba02275445dd9
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-13 07:49:04 +0200

    Create #029-Objects with Parameterized Constructor and Array.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-OOP(Concepts-Practice)/#029-Objects with Parameterized Constructor and Array.cpp
@@ -0,0 +1,47 @@
+//ProgrammingAdivces.com
+//Mohammed Abu-Hadhoud
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class clsA
+{
+public:
+
+	//Parametarized Constructor
+	clsA(int value)
+	{
+		x = value;
+	}
+
+	int x;
+
+	void Print()
+	{
+		cout << "The value of x=" << x <<endl;
+	}
+
+};
+
+
+int main() 
+
+{
+	
+	// Initializing 3 array Objects with function calls of
+    // parameterized constructor as elements of that array
+	clsA obj[] = { clsA(10), clsA(20), clsA(30) };
+
+	// using print method for each of three elements.
+	for (int i = 0; i < 3; i++) {
+		obj[i].Print();
+	}
+
+	return 0;
+
+	
+	system("pause>0");
+
+}
+
