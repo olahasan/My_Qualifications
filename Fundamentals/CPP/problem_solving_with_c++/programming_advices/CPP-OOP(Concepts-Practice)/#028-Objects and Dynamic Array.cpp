810bbc56eae2b33f54f95ccae4d70daed3875d5c
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-13 07:43:26 +0200

    Create #028-Objects and Dynamic Array.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-OOP(Concepts-Practice)/#028-Objects and Dynamic Array.cpp
@@ -0,0 +1,58 @@
+//ProgrammingAdivces.com
+//Mohammed Abu-Hadhoud
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class clsA
+{
+public:
+
+	// dummy constructor
+	clsA() {}
+
+	//Parametarized Constructor
+	clsA(int value)
+	{
+		x = value;
+	}
+
+	int x;
+
+	void Print()
+	{
+		cout << "The value of x=" << x <<endl;
+	}
+
+};
+
+
+int main() 
+
+{
+	short NumberOfObjects = 5;
+
+	// allocating dynamic array
+   // of Size NumberOfObjects using new keyword
+
+	clsA * arrA = new clsA[NumberOfObjects];
+
+	// calling constructor
+	// for each index of array
+	for (int i = 0; i < NumberOfObjects; i++) {
+		arrA[i] = clsA(i);
+	}
+
+	// printing contents of array
+	for (int i = 0; i < NumberOfObjects; i++) {
+		arrA[i].Print();
+	}
+
+	return 0;
+
+	
+	system("pause>0");
+
+}
+
