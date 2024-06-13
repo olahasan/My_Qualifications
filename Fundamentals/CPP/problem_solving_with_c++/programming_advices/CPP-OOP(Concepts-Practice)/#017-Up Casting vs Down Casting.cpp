1bc09508f7fc6ba59135ba529ff18be8dc389f45
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-13 06:21:37 +0200

    Create #017-Up Casting vs Down Casting.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-OOP(Concepts-Practice)/#017-Up Casting vs Down Casting.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+class clsPerson
+{
+
+public:
+
+    string FullName = "Mohammed Abu-Hadhoud";
+
+};
+
+class clsEmployee : public clsPerson
+{
+
+public:
+    string Title = "CEO";
+
+};
+
+int main()
+
+{
+
+    clsEmployee Employee1;
+
+    cout << Employee1.FullName << endl;
+
+    ////upcasting
+    //this will convert employee to person.
+    clsPerson* Person1 = &Employee1;
+    cout << Person1->FullName << endl;
+
+
+    //clsPerson Person2;
+    //cout << Person2.FullName << endl;
+
+    ////downcasting : you cannot convert person to employee
+    // 
+    //clsEmployee* Employee2 = &Person2;
+
+
+
+    system("pause>0");
+    return 0;
+}
