07a60d1c3d87d072b497d75f3723cc4675ea9682
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-14 08:10:17 +0200

    Create #002-STL-Stack.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Data Structures - Level1/#002-STL-Stack.cpp
@@ -0,0 +1,37 @@
+// ProgrammingAdvices.com
+// Mohammed Abu-Hadhoud
+#include <iostream>
+#include <stack>
+
+
+using namespace std;
+
+int main()
+
+{
+    // create a stack of ints
+    stack <int> stkNumbers;
+
+    // push into stack
+    stkNumbers.push(10);
+    stkNumbers.push(20);
+    stkNumbers.push(30);
+    stkNumbers.push(40);
+    stkNumbers.push(50);
+
+    // we can access the element by getting the top and popping
+    // until the stack is empty
+    cout << "count=" << stkNumbers.size() << endl;
+    
+    cout << "Numbers are:\n";
+    while (!stkNumbers.empty()) {
+        // print top element
+        cout << stkNumbers.top() << "\n";
+
+        // pop top element from stack
+        stkNumbers.pop();
+    }
+
+    system("pause>0");
+    return 0;
+}
