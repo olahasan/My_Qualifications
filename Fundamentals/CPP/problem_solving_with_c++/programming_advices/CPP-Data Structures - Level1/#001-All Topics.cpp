894f716a5dda46b14b2a3babc9275479b6d11c15
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-14 08:05:52 +0200

    Create #001-All Topics.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Data Structures - Level1/#001-All Topics.cpp
@@ -0,0 +1,15 @@
+1-  What is Data Structure? and Why we use it?
+2-  Differences between Data Structures and Database.
+3- What are the Classification/Types of Data Structures?
+4- Time & Speed Complexity- Big O
+  4-1- Things Affect Program Speed & Efficiency
+  4-2- Time & Space Complexity - Big O Notation
+    4-2-1- Big O(1) : Constant Time Function
+    4-2-2- Big O(n) : Linear Time Function
+    4-2-3- Big O(n^2) : Quadratic Time Function
+    4-2-4- Big O(Log n) : Logarithmic Time Function
+5- Binary Data Structure
+6- Array  Data Structure
+7- Matrix Data Structure
+8- Stack Data Structure
+9- 
