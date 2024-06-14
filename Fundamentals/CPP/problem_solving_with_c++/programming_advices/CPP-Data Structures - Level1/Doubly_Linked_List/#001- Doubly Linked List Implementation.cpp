d8ed17068cb5b85f2026e7cf9be8bb59648cc9a6
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-14 09:50:03 +0200

    Create #001- Doubly Linked List Implementation.cpp

---
+++ Fundamentals/CPP/problem_solving_with_c++/programming_advices/CPP-Data Structures - Level1/Doubly_Linked_List/#001- Doubly Linked List Implementation.cpp
@@ -0,0 +1,61 @@
+// ProgrammingAdvices.com
+// Mohammed Abu-Hadhoud
+#include <iostream>
+
+
+
+using namespace std;
+
+// Creating a node
+class Node
+
+{
+public:
+    int value;
+    Node* next;
+    Node* prev;
+};
+
+int main()
+
+{
+
+    Node* head;
+
+    Node* Node1 = NULL;
+    Node* Node2 = NULL;
+    Node* Node3 = NULL;
+
+    // allocate 3 nodes in the heap
+    Node1 = new Node();
+    Node2 = new Node();
+    Node3 = new Node();
+
+    // Assign value values
+    Node1->value = 1;
+    Node2->value = 2;
+    Node3->value = 3;
+
+    // Connect nodes
+    Node1->next = Node2;
+    Node1->prev = NULL;
+
+    Node2->next = Node3;
+    Node2->prev = Node1;
+
+    Node3->next = NULL;
+    Node3->prev = Node2;
+
+    // print the linked list value
+    head = Node1;
+
+    while (head != NULL) {
+        cout << head->value << endl;
+        head = head->next;
+    }
+
+    system("pause>0");
+    return 0;
+
+}
+
