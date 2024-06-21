a341d0fdeb1beecb5114989a129b0772b41c4974
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-21 15:03:12 +0200

    Update C# 102.cs

--- Back-End/C#/satr.codes/C# 102/C# 102.cs
+++ Back-End/C#/satr.codes/C# 102/C# 102.cs
@@ -1,3 +1,24 @@
 1- Numbers:
-          - Date and Time
-          - converting Date Types  
+          - Date and Time  new DateTime();
+          - converting Date Types   implicit , explicit
+                    
+2- Operators:
+          - Arithmetic Operators: + - * / %
+          - Logical Operators: && || !
+          - Comparison Operators: == != >= <= < >
+          - increment ++  decrement --  
+
+3- topics in c#:
+          - parsing
+          - enum
+          - reference & value
+          - out
+          - ref
+
+4- libraries:
+          - Math library
+
+5- class:  
+         - object
+         - Fields
+         - Constructor
