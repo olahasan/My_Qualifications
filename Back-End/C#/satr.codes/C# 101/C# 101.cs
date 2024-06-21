a52ff43a00a656ea75c5b862d9715d9584944478
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-21 10:12:56 +0200

    Create C# 101.cs

---
+++ Back-End/C#/satr.codes/C# 101/C# 101.cs
@@ -0,0 +1,42 @@
+1- intro:
+        - install 
+        - starts with "COOL" name that mean : "C- like Object Oriented Language"
+          
+2- fundamentals: 
+        - print statement: Console.WriteLine("hello");
+                           Console.ReadKey();
+        - declare variables and use it
+        - input statement: Console.ReadLine();
+        - Comments: // or /* */
+
+3- Data Types:
+        - string
+        - int 
+        - double
+        - bool
+   = variables and constants    
+
+ 4- Conditions:
+        - If statement
+        - else statement
+        - If ... else statement
+        - switch statement
+
+5- loop:
+        - for loop
+        - do ... while loop
+        - while loop
+    = infinite loop
+
+6- Arrays:
+        - declare and add items
+        - print array using foreach
+
+7- Functions:
+        - declare and Call
+        - arguments & parameters
+        - return 
+          
+
+
+ 
