91f06d882c2993d343368d26060c3b5085171450
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-22 15:06:48 +0200

    Update and rename C#-Level_1 to C#-Level_1.cs

---
+++ Back-End/C#/programming_advices/C#-Level_1.cs
@@ -0,0 +1,173 @@
+1- the theoretical side:
+       - Library , Framework and Platform 
+       - .NET Core vs .NET Framework
+       - Compilation in .NET:  interpreter(Jit), Compiled(Head of time)
+       - .NET Framework Architecture : 
+                               - class library: 
+                                        contains thousands of classes that supports alot of functions that make:
+                                              - Support exceptions handling            
+                                              - Support for creating web services
+                                              - create web-client and server applications
+                                              -  user-defined data types
+                                              - ...........
+                                  
+                               - CLR (common language library): 
+                                         = CLR main components: 
+                                                      - Common Type System (CTS): Provides the data types using managed code. int32  
+                                                      - Common Language Speciation (CLS):  ensures interoperability among applications
+                                                      - Garbage Collection (GC): an automatic memory manager
+                                                      - just in time (JIT)compiler:  
+                                                                              - JIT types:
+                                                                                          - normal JIT (default)
+                                                                                          - pre compiler JIT (AOT) A head Of Time
+                                                                                          - Econo JIT
+                                                      - metadata and assemblies: 
+                                                                              - assemblies types:
+                                                                                          - Process Assembly (.exe file)
+                                                                                          - library Assembly (dll file)
+            
+                                         = CLR functions:
+                                                      - converts the program into native code  
+                                                      - Handles Exceptions
+                                                      - Memory management
+                                                      - Garbage collection
+                                                      - Manager
+                                                      - .........
+            
+                                         = CLR Structure:
+                                                      - Base Class Library
+                                                      - Thread Support
+                                                      - Security Engine
+                                                      - Type Checker 
+                                                      - Garbage Collector
+                                                      - ..........  
+
+                    ========================================================================================                                                                  
+
+2- Syntax:
+       - Output:
+           - Console.WriteLine(); 
+           - Console.Write();
+           - Formatted String  Console.WriteLine("Hi My Name is {0} and i live in {1} ", name, city);
+           - Escape Characters:
+                    - newLine \n
+                    - tap \t
+                    - Backspace \b
+                    - Single quote \'
+                    - Double quote \"
+                    - Backslash \\
+                    - Alert \a 
+
+       - Comments:
+           - Single Line //
+           - Multiple Lines Comments /* */ 
+
+      - Variables:
+
+      - DataTypes:
+           - value type
+           - reference type
+           - Numbers Datatypes
+                  - Integer Types
+                  - Floting Types
+           - Default Values
+           - Enums
+           - Nullable Types
+           - Anonymous Type
+           - Structures
+           - Dynamic Type 
+     
+      - Working with Date & Time:
+           - Set Date & Time
+           - Get Current Datetime
+           - DateTime Static Fields
+           - Time Span
+           - Convert String to DateTime
+
+      - Strings
+
+      - Type Casting:
+           - Implicit Casting
+           - Explicit Casting
+           - Type Conversion Methods: Convert.ToBoolean(); Convert.ToString(); .........
+
+      - User Input:
+           - Console.ReadLine();
+
+      - Operators:
+           - Assignment Operators: += -= *= /= ....
+           - Arithmetic Operators: + - * / %
+           - Relational Operators: == != >= <= > <
+           - Logical Operators: && || !
+           - Unary Operators: + - ++ -- !
+           - Ternary Operator: Condition? Expression1 : Expression2;
+           - Bitwise and Bit Shift Operators: & | << >>
+
+      - Control Flow:
+           - if    if...else    if...else if
+           - switch Statement
+           - ternary (? :) Operator
+           - for loop
+           - while loop 
+           - do while loop
+           - break Statement
+           - continue Statement
+
+      - Arrays:
+           - Array Declaration
+           - Array initialization
+           - Access Array Elements
+           - 2 Dimensional Array
+           -  for each loop
+           
+      - Array Operations using System.Linq:
+           - Max(), Min(), Count(), Sum(), Average()
+
+      - Math:
+           - Math.Max(x,y), Min(x,y), Sqrt(x), Abs(x), Round()
+
+      - Methods:
+           - Parameters and Arguments
+           - Default Parameter Value
+           - Return Values
+           - Named Arguments
+           - Method Overloading
+           
+      - Exceptions:
+           - try ... catch
+
+      - Misc:
+           - Generate Random Number 
+
+                    ========================================================================================                                                                  
+
+3- Windows Forms:
+           - Controls
+           - textbox, button, form, labal
+           - TabIndex, Tabstop, and New Forms (Default, Show(), ShowDialog(), Close)
+           - MessageBox, CheckBox, RadioButton, GroupBox, and Tag
+           - textbox: Focus, MaxLength, Password, RightToLeft, Locked,Multiline, WordWrap,dock
+           - PictureBox
+           - Drawing: Draw Line , ellipse , and rectangle.
+           - MaskedTextBox, ComboBox, 
+           - LinkLable, CheckedListBox
+           - DateTimePicker, MonthCalandar
+           - Timer, NotifyIcon, TreeView and ImageList, ProgressBar, ListView, defaultbutton
+           - Error Provider, TrackBar, NumericUpDown
+           - Containers: TabControl, GroupBox vs Panel
+           - Dialogs:  ColorDialog, FontDialog, FileSaveDialog, FileOpenDialog, FolderBrowserDialog
+           - Multiple Documents Interface (MDI) Container
+           - Menus: MenuStrip, Context Menu
+
+
+           
+
+
+
+
+
+       
+
+
+
+    
