6cb47999c41364ac6d479a6bbc2dedc3ad5132d5
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-06-23 17:19:07 +0200

    Create w3schools- C# Quiz.cs

---
+++ Back-End/C#/w3schools/w3schools- C# Quiz.cs
@@ -0,0 +1,124 @@
+Question 1:
+What is a correct syntax to output "Hello World" in C#?
+  
+Console.WriteLine("Hello World"); 
+====================================================================================
+Question 2:
+C# is an alias of C++
+
+False    
+====================================================================================
+Question 3:
+How do you insert COMMENTS in C# code?
+
+// This is a comment  
+====================================================================================
+Question 4:
+Which data type is used to create a variable that should store text?
+
+string    
+====================================================================================
+Question 5:
+How do you create a variable with the numeric value 5?
+
+int x = 5; 
+====================================================================================
+Question 6:
+How do you create a variable with the floating number 2.8?
+
+double x = 2.8D;   
+====================================================================================
+Question 7:
+Which property can be used to find the length of a string?
+
+Length   
+====================================================================================
+Question 8:
+Which operator is used to add together two values?
+
+The + sign  
+====================================================================================
+Question 9:
+The value of a string variable can be surrounded by single quotes.
+
+False 
+====================================================================================
+Question 10:
+Type casting is when you assign a value of one data type to another type.
+
+True  
+====================================================================================
+Question 11:
+Which operator can be used to compare two values?
+
+==
+====================================================================================
+Question 12:
+To declare an array in C#, define the variable type with:
+
+[]    
+====================================================================================
+Question 13:
+Array indexes start with:
+
+0 
+====================================================================================
+Question 14:
+How do you create a method in C#?
+
+MyMethod()   
+====================================================================================
+Question 15:
+How do you call a method in C#?
+
+MyMethod();  
+====================================================================================
+Question 16:
+Which keyword is used to create a class in C#?
+
+class   
+====================================================================================
+Question 17:
+What is the correct way to create an object called myObj of MyClass?
+
+MyClass myObj = new MyClass();   
+====================================================================================
+Question 18:
+In C#, it is possible to inherit fields and methods from one class to another.
+
+True   
+====================================================================================
+Question 19:
+What is the name of the 'special' class that represents a group of constants?
+
+enum 
+====================================================================================
+Question 20:
+Which operator is used to multiply numbers?
+
+*   
+====================================================================================
+Question 21:
+Which access modifier makes the code only accessible within the same class?
+
+private   
+====================================================================================
+Question 22:
+How do you start writing an if statement in C#?
+
+if (x > y)   
+====================================================================================
+Question 23:
+How do you start writing a while loop in C#?
+
+while (x > y)  
+====================================================================================
+Question 24:
+Which keyword is used to return a value inside a method?
+
+return   
+====================================================================================
+Question 25:
+Which statement is used to stop a loop?
+
+break   
