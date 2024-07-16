b370038db3afeb68d05470806ec18341d8dd48d9
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-16 14:22:50 +0200

    Create #003-Questions-lessons from 18 to 22_Operators.js

---
+++ Front-End/Javascript/elzero.org/Assignments/#003-Questions-lessons from 18 to 22_Operators.js
@@ -0,0 +1,44 @@
+https://codepen.io/olahasan/pen/PoRVVKr
+https://elzero.org/javascript-bootcamp-assignments-lesson-from-018-to-022/
+
+first question
+
+console.log(10*20-15%3+190+10-400);
+/////////////////////////////////////////////////////////////////////////////
+second question
+
+let num = 3;
+console.log(num+num);//6
+
+console.log(num*num-num);//6
+
+console.log(num++ + num++ + -true);//6
+
+console.log(++num + num++ + -true +false*true -true);//6
+
+console.log(++num + --num + -true);//6
+
+console.log(num * ++num - num++ -true + -true);//6
+
+console.log(num * ++num - ++num -true);//6
+/////////////////////////////////////////////////////////////////////////////
+third question
+
+let num = "10";
+console.log(+num + +num);
+
+console.log(+num +num++ + -(--num) +num);
+
+console.log(num++ + --num);
+
+console.log(+num + num*num / num + +num  -num);
+
+console.log(+num + num*num / num +  -num + +num  -num + num++ + --num  -num);
+
+
+console.log(+num * +num -num -num -num -num -num -num -num -num );
+/////////////////////////////////////////////////////////////////////////////
+fourth question
+1)++points + points++ + true
+
+2)--points -true
