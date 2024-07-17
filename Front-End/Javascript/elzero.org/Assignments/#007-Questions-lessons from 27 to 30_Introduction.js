4088a8d6f9c463f33888012695072a02be2b0dc1
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-17 08:22:37 +0200

    Create #007-Questions-lessons from 27 to 30_Introduction.js

---
+++ Front-End/Javascript/elzero.org/Assignments/#007-Questions-lessons from 27 to 30_Introduction.js
@@ -0,0 +1,31 @@
+====>>>> https://codepen.io/olahasan/pen/eYMxxjG
+====>>>> https://elzero.org/javascript-bootcamp-assignments-lesson-from-027-to-030/
+
+//first question
+
+let userName = "Elzero";
+console.log(userName[0].toLowerCase()); // e
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName.slice(0, 1).toLowerCase()); // e
+console.log(userName.substring(0, 1).toLowerCase()); // e
+console.log(userName.substring(userName.length-6, userName.length-5).toLowerCase()); // e
+console.log(userName.substr(0,1).toLowerCase()); // e
+console.log(userName.slice(-6, -5).toLowerCase().repeat(3)); // eee
+////////////////////////////////////////////////////////////////////////////////
+//second question
+
+let word = "Elzero";
+let letterZ = "z";
+let letterE = "e";
+let letterO = "O";
+
+console.log(word.includes(letterZ)); // True
+console.log(word.startsWith(letterE.toUpperCase())); // True
+console.log(word.endsWith(letterO.toLocaleLowerCase())); // True
+////////////////////////////////////////////////////////////////////////////////
+//third question
+////////////////////////////////////////////////////////////////////////////////
+//fourth question
+////////////////////////////////////////////////////////////////////////////////
+//fifth question
+////////////////////////////////////////////////////////////////////////////////
