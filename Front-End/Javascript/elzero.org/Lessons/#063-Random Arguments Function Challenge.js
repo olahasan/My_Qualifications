50c5fdf33f1f339ff599630a72f92ce909d285fb
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-18 18:35:11 +0200

    Create #063-Random Arguments Function Challenge.js

---
+++ Front-End/Javascript/elzero.org/Lessons/#063-Random Arguments Function Challenge.js
@@ -0,0 +1,18 @@
+/*
+  Function - Random Argument Challenge
+  ====================================
+  Create Function showDetails
+  Function Accept 3 Parameters [a, b, c]
+  Data Types For Info Is
+  - String => Name
+  - Number => Age
+  - Boolean => Status
+  Argument Is Random
+  Data Is Not Sorted Output Depend On Data Types
+  - Use Ternary Conditional Operator
+*/
+
+showDetails("Osama", 38, true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(38, "Osama", true); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(true, 38, "Osama"); // "Hello Osama, Your Age Is 38, You Are Available For Hire"
+showDetails(false, "Osama", 38); // "Hello Osama, Your Age Is 38, You Are Not Available For Hire"
