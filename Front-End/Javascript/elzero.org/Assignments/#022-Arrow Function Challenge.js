a923a27ce4bdf13d9903712550722226b5bc24cc
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-18 20:41:14 +0200

    Create #022-Arrow Function Challenge.js

---
+++ Front-End/Javascript/elzero.org/Assignments/#022-Arrow Function Challenge.js
@@ -0,0 +1,12 @@
+// التحدي الأول
+
+let names = function(...names){
+
+  // return para
+  return `// string [${names.join("],[")}] =>Done`
+};
+
+console.log(names("Osama", "Mohamed", "Ali", "Ibrahim"));
+// string [Osama], [Mohamed], [Ali], [Ibrahim], =>Done !
+
+// التحدي الثاني
