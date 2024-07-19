54ba82ecf176a89f2d6f0aa6cbb48b4571cc1956
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-19 18:20:43 +0200

    Create #091-Deal With Childrens.js

---
+++ Front-End/Javascript/elzero.org/Lessons/#091-Deal With Childrens.js
@@ -0,0 +1,38 @@
+// html
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <title>Learn JavaScript</title>
+  </head>
+  <body>
+    <div><!-- Osama -->Hello Div<p>Hello P</p><span>Hello Span</span><!-- Comment -->Hello</div>
+    <script src="main.js"></script>
+  </body>
+</html>
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// js
+/*
+  DOM [Deal With Childrens]
+  - children
+  - childNodes
+  - firstChild
+  - lastChild
+  - firstElementChild
+  - lastElementChild
+*/
+
+let myElement = document.querySelector("div");
+
+console.log(myElement);
+console.log(myElement.children);
+console.log(myElement.children[0]);
+console.log(myElement.childNodes);
+console.log(myElement.childNodes[0]);
+
+console.log(myElement.firstChild);
+console.log(myElement.lastChild);
+
+console.log(myElement.firstElementChild);
+console.log(myElement.lastElementChild);
