7037610c6feefb2e1dd0634ac96018f0dc5c453b
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-24 20:10:40 +0200

    Create #141-Regular Expression – Quantifiers Part 2.js

---
+++ Front-End/Javascript/elzero.org/Lessons/#141-Regular Expression – Quantifiers Part 2.js
@@ -0,0 +1,14 @@
+/*
+  Regular Expression
+
+  Quantifiers
+  n{x}   => Number of
+  n{x,y} => Range
+  n{x,}  => At Least x
+*/
+
+let serials = "S100S S3000S S50000S S950000S";
+
+console.log(serials.match(/s\d{3}s/ig)); // S[Three Number]S
+console.log(serials.match(/s\d{4,5}s/ig)); // S[Four Or Five Number]S
+console.log(serials.match(/s\d{4,}s/ig)); // S[At Least Four]S
