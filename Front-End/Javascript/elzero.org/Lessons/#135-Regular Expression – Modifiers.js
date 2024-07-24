b161fe9d9ec1adeb66463dcdf63a9c324d9d2fbc
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-24 19:45:12 +0200

    Create #135-Regular Expression – Modifiers.js

---
+++ Front-End/Javascript/elzero.org/Lessons/#135-Regular Expression – Modifiers.js
@@ -0,0 +1,26 @@
+/*
+  Regular Expression
+
+  Syntax
+  /pattern/modifier(s);
+  new RegExp("pattern", "modifier(s)")
+
+  Modifiers => Flags
+  i => case-insensitive
+  g => global
+  m => Multilines
+
+  Search Methods
+  - match(Pattern)
+
+  Match
+  -- Matches A String Against a Regular Expression Pattern
+  -- Returns An Array With The Matches
+  -- Returns null If No Match Is Found.
+*/
+
+let myString = "Hello Elzero Web School I Love elzero";
+
+let regex = /elzero/ig;
+
+console.log(myString.match(regex));
