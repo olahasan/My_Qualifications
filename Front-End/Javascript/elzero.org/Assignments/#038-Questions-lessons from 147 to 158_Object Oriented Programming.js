e8700357ed107ed2813aa25a1dc881141af7e48d
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-25 14:33:31 +0200

    Update #038-Questions-lessons from 147 to 158_Object Oriented Programming.js

--- Front-End/Javascript/elzero.org/Assignments/#038-Questions-lessons from 147 to 158_Object Oriented Programming.js
+++ Front-End/Javascript/elzero.org/Assignments/#038-Questions-lessons from 147 to 158_Object Oriented Programming.js
@@ -71,6 +71,49 @@ console.log(`${TabletThree.fullDetails()}`);
 
 //third question
 
+// Edit The Class
+class User {
+  #c;
+  constructor(username, card) {
+    this.u = username;
+    this.#c = card;
+  }
+
+  get showData() {
+    return `Hello ${this.u} Your Credit Card Number Is ${this.formatCardNumber(
+      this.#c
+    )}`;
+  }
+
+  formatCardNumber(cardNumber) {
+    if (/^\d{4}-\d{4}-\d{4}-\d{4}$/.test(cardNumber)) {
+      return cardNumber;
+    } else {
+      const formattedCardNumber = cardNumber
+        .toString()
+        .replace(/(.{4})/g, "$1-");
+      return formattedCardNumber.slice(0, -1);
+    }
+  }
+}
+
+// Do Not Edit Anything Below
+
+let userOne = new User("Osama", "1234-5678-1234-5678");
+let userTwo = new User("Ahmed", "1234567812345678");
+let userThree = new User("Ghareeb", 1234567812345678);
+
+console.log(userOne.showData);
+// Hello Osama Your Credit Card Number Is 1234-5678-1234-5678
+
+console.log(userTwo.showData);
+// Hello Ahmed Your Credit Card Number Is 1234-5678-1234-5678
+
+console.log(userThree.showData);
+// Hello Ghareeb Your Credit Card Number Is 1234-5678-1234-5678
+
+console.log(userOne.c); // Prevent Accessing To Card Property Here
+// Undefined
 
 ////////////////////////////////////////////////////////////////////////////////­­­­
 fourth question
