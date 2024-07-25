62b09c175c6b33e562a5426640098fa6a1cc863a
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-25 13:43:19 +0200

    Create #038-Questions-lessons from 147 to 158_Object Oriented Programming.js

---
+++ Front-End/Javascript/elzero.org/Assignments/#038-Questions-lessons from 147 to 158_Object Oriented Programming.js
@@ -0,0 +1,78 @@
+====>>>> 
+====>>>>­­
+
+
+//first question
+
+class Car {
+  constructor(name, model, price) {
+    this.n = name;
+    this.m = model;
+    this.p = price;
+  }
+  run() {
+    return "Car Is Running Now";
+  }
+  stop() {
+    return "Car Is Stopped";
+  }
+}
+
+let car1 = new Car("MG", 2022, 420000);
+let car2 = new Car("volvo", 2023, 440000);
+let car3 = new Car("BMW", 2024, 460000);
+
+console.log(`Car One Name Is ${car1.n} And Model Is ${car1.m} And Price Is ${car1.p}`);
+console.log(car1.run());
+
+// Needed Output
+
+("Car One Name Is MG And Model Is 2022 And Price Is 420000");
+("Car Is Running Now");
+
+////////////////////////////////////////////////////////////////////////////////
+
+//second question
+
+class Phone {
+  constructor(name, serial, price) {
+    this.name = name;
+    this.serial = serial;
+    this.price = price;
+  }
+}
+
+// Write Tablet Class Here
+class Tablet extends Phone {
+  constructor(name, serial, price, size) {
+    super(name, serial, price);
+    this.size = size || "Unknown";
+  }
+
+  fullDetails() {
+    return `${Tablet.name} Serial is ${Tablet.serial} And Size Is ${this.size}`;
+  }
+}
+
+let TabletOne = new Tablet("iPad", 100200300, 1500, 12.9);
+let TabletTwo = new Tablet("Nokia", 350450650, 800, 10.5);
+let TabletThree = new Tablet("LG", 250450650, 650);
+
+console.log(`${TabletOne.fullDetails()}`);
+// iPad Serial is 100200300 And Size Is 12.9
+
+console.log(`${TabletTwo.fullDetails()}`);
+// Nokia Serial is 350450650 And Size Is 10.5
+
+console.log(`${TabletThree.fullDetails()}`);
+// LG Serial is 250450650 And Size Is Unknown
+
+////////////////////////////////////////////////////////////////////////////////
+
+//third question
+
+
+////////////////////////////////////////////////////////////////////////////////­­­­
+fourth question
+////////////////////////////////////////////////////////////////////////////////
+fifth question
