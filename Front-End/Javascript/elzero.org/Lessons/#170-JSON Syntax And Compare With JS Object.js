aa4aebfc579847f6d6c2b1ccd0c54adb2e1b8e62
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-26 17:50:38 +0200

    Create #170-JSON Syntax And Compare With JS Object.js

---
+++ Front-End/Javascript/elzero.org/Lessons/#170-JSON Syntax And Compare With JS Object.js
@@ -0,0 +1,30 @@
+//main.js
+
+/*
+  JSON Syntax
+  - Data Added Inside Curly Braces {  }
+  - Data Added With Key : Value
+  - Key Should Be String Wrapped In Double Quotes
+  - Data Separated By Comma
+  - Square Brackets [] For Arrays
+  - Curly Braces {} For Objects
+
+  Available Data Types
+  - String
+  - Number
+  - Object
+  - Array
+  - Boolean Values
+  - null
+*/
+/////////////////////////////////////////////////////////////////
+//test.json
+
+{
+  "string": "Elzero",
+  "number": 100,
+  "object": { "EG": "Giza", "KSA": "Riyadh" },
+  "array": ["HTML", "CSS", "JS"],
+  "boolean": true,
+  "null": null
+}
