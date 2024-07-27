6a4d0da3cf117cbd24bf693c3854e08aa0f26e89
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-27 17:10:51 +0200

    Create #041-Questions-lessons from 179 to 188_Promises.js

---
+++ Front-End/Javascript/elzero.org/Assignments/#041-Questions-lessons from 179 to 188_Promises.js
@@ -0,0 +1,9 @@
+====>>>> https://codepen.io/olahasan/pen/xxWeKYX
+====>>>>­­ https://elzero.org/javascript-bootcamp-assignments-lesson-from-179-to-188/
+
+
+//first question
+
+
+////////////////////////////////////////////////////////////////////////////////
+//second question
