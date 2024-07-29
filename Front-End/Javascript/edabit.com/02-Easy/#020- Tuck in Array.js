9417b0ebd72d272ea5eec5a7c9c616bc9ed75427
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-29 20:37:21 +0200

    Update #020- Tuck in Array.js

--- Front-End/Javascript/edabit.com/02-Easy/#020- Tuck in Array.js
+++ Front-End/Javascript/edabit.com/02-Easy/#020- Tuck in Array.js
@@ -9,6 +9,13 @@ function tuckIn(arr1, arr2) {
   return newArr;
 }
 
+////////////////////////////////////////////////////////////
+//or
+function tuckIn(arr1, arr2) {
+   arr1.splice(arr1.length / 2, 0, arr2);
+  return arr1;
+}
+
 console.log(tuckIn([1, 10], [2, 3, 4, 5, 6, 7, 8, 9])); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 console.log(tuckIn([15, 150], [45, 75, 35])); //[15, 45, 75, 35, 150]
