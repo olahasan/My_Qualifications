1564557c461ebd8633faa03ac65102c1857fd3f9
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-30 20:36:12 +0200

    Create #039- Pair Management.js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#039- Pair Management.js
@@ -0,0 +1,7 @@
+function makePair(num1, num2) {
+  return [num1, num2];
+}
+
+console.log(makePair(1, 2)); // [1, 2]
+console.log(makePair(51, 21)); // [51, 21]
+console.log(makePair(512124, 215)); // [512124, 215]
