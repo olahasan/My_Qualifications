0e4db597f6d31be59f11976a9f606489dc006029
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-30 19:34:26 +0200

    Create #032- Using Arrow Functions.js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#032- Using Arrow Functions.js
@@ -0,0 +1,9 @@
+function arrowFunc(num){ //normal function
+  return num;
+}
+
+const arrowFunc = (num) => num; //arrow function
+
+console.log(arrowFunc(3)); // 3
+console.log(arrowFunc("3")); // "3"
+console.log(arrowFunc(true)); //true
