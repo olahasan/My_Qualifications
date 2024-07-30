467ff36f57baec54586a9303b0ef29b08fc7e200
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-30 20:07:21 +0200

    Create #035- Buggy Code (Part 4).js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#035- Buggy Code (Part 4).js
@@ -0,0 +1,10 @@
+function greeting(name) {
+  if (name == "Mubashir") {
+    return "Hello, my Love!";
+  }
+  return "Hello, " + name + "!";
+}
+
+console.log(greeting("Matt")); // "Hello, Matt!"
+console.log(greeting("Helen")); // "Hello, Helen!"
+console.log(greeting("Mubashir")); // "Hello, my Love!"
