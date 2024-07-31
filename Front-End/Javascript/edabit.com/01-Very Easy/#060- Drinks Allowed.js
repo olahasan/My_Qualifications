156526a0f4d52f73a52ba9408ba6c64848a9cd11
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-31 21:51:40 +0200

    Create #060- Drinks Allowed.js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#060- Drinks Allowed.js
@@ -0,0 +1,7 @@
+function shouldServeDrinks(age, onBreak) {
+  return onBreak === true ? false : age > 18 ? true : false;
+}
+
+console.log(shouldServeDrinks(17, true)); //➞ false
+console.log(shouldServeDrinks(19, false)); //➞ true
+console.log(shouldServeDrinks(30, true)); // ➞ false
