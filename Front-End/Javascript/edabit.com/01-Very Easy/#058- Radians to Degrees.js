2e730540e014ce20ca461624c8a4f87b39c07958
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-31 21:33:57 +0200

    Create #058- Radians to Degrees.js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#058- Radians to Degrees.js
@@ -0,0 +1,8 @@
+function radiansToDegrees(rad) {
+  const fixed = 57.29577951308232;
+  return fixed * rad;
+}
+
+console.log(radiansToDegrees(1)); // ➞ 57.29577951308232
+console.log(radiansToDegrees(20)); // ➞ 1145.9155902616465
+console.log(radiansToDegrees(50)); // ➞ 2864.7889756541163
