7f9d0cfd082216a976deace087afa4daf653b256
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-31 21:16:26 +0200

    Create #056- Reverse an Array.js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#056- Reverse an Array.js
@@ -0,0 +1,7 @@
+function reverse(arr) {
+  return arr.reverse();
+}
+
+console.log(reverse([1, 2, 3, 4])); // ➞ [4, 3, 2, 1]
+console.log(reverse([9, 9, 2, 3, 4])); // ➞ [4, 3, 2, 9, 9]
+console.log(reverse([])); //  ➞ []
