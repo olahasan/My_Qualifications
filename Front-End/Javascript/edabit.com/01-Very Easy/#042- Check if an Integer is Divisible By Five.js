f776a9d624f793484a8766c778ba52ea1060ea14
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-31 19:02:33 +0200

    Create #042- Check if an Integer is Divisible By Five.js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#042- Check if an Integer is Divisible By Five.js
@@ -0,0 +1,7 @@
+function divisibleByFive(n) {
+  return n % 5 === 0;
+}
+console.log(divisibleByFive(5)); //  ➞ true
+console.log(divisibleByFive(-55)); //  ➞ true
+console.log(divisibleByFive(37)); // ➞ false
+console.log(divisibleByFive(25)); // ➞ true
