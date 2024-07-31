859bf9873987f1dbb08fa86126b0fc496ba0dc5c
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-07-31 19:16:40 +0200

    Create #044- Recursion: Length of a String.js

---
+++ Front-End/Javascript/edabit.com/01-Very Easy/#044- Recursion: Length of a String.js
@@ -0,0 +1,8 @@
+function length(str) {
+  return str.length;
+}
+
+console.log(length("apple")); //  ➞ 5
+console.log(length("make")); //  ➞ 4
+console.log(length("a")); // ➞ 1
+console.log(length("")); // ➞ 0
