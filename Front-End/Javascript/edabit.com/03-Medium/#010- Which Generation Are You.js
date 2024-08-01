b876e9f271a4e6ed03d40cc110e4f4fbd2d9a408
Ola Ali <92292843+olahasan@users.noreply.github.com>
2024-08-01 16:16:07 +0200

    Update #010- Which Generation Are You.js

--- Front-End/Javascript/edabit.com/03-Medium/#010- Which Generation Are You.js
+++ Front-End/Javascript/edabit.com/03-Medium/#010- Which Generation Are You.js
@@ -50,3 +50,28 @@ function generation(x, y) {
 console.log(generation(2, "f")); //➞ "granddaughter"
 console.log(generation(-3, "m")); //➞ "great grandfather"
 console.log(generation(1, "f")); // ➞ "daughter"
+
+//////////////////////////////////////////////////////////////////////////////
+//other way
+function generation(x, y) {
+  const generations = {
+    "-3": { m: "great grandfather", f: "great grandmother" },
+    "-2": { m: "grandfather", f: "grandmother" },
+    "-1": { m: "father", f: "mother" },
+    0: "me!",
+    1: { m: "son", f: "daughter" },
+    2: { m: "grandson", f: "granddaughter" },
+    3: { m: "great grandson", f: "great granddaughter" },
+  };
+
+  if (x in generations) {
+    return y === "m" ? generations[x].m : generations[x].f;
+  } else {
+    return "Invalid input";
+  }
+}
+
+console.log(generation(2, "f")); // ➞ "granddaughter"
+console.log(generation(-3, "m")); // ➞ "great grandfather"
+console.log(generation(1, "f")); // ➞ "daughter"
+console.log(generation(0, "m")); // ➞ "me!"
